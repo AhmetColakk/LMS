6309deb1b4872903e2e8e7593269fc65bec17897
AhmetColakk <pxahmetcolak@gmail.com>
2023-01-05 23:23:36 +0200

    fifth commit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 const cookieParser = require('cookie-parser');
 const studentRouter = require('./routes/studentRouter');
 const announcementRouter = require('./routes/announcementRouter');
@@ -11,7 +10,6 @@ const studentAuhtRouter = require('./routes/authStudent');
 const { Server } = require('socket.io');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
-const Announcement = require('./model/announcementModel');
 const onSocket = require('./socket/socket');
 const onMeeting = require('./socket/meeting');
 const path = require('path');
@@ -20,6 +18,15 @@ const path = require('path');
 const app = express();
 
 // ? set graphql
+
+// ? Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
 app.use(
   '/graphql',
   graphqlHTTP({
@@ -28,23 +35,15 @@ app.use(
   }),
 );
 
-// ? Middlewares
-// app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(express.static('public'));
-
 // app.use(requireAuth);
 app.use('/studentAPI', studentRouter);
 app.use('/announcementAPI', announcementRouter);
 app.use('/studentAuth', studentAuhtRouter);
 app.use('/teacherAPI', teacherRouter);
 
-app.get('*', (req, res) =>
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html')),
-);
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL;
@@ -55,10 +54,8 @@ const io = new Server(4000, {
     methods: ['GET', 'POST'],
   },
 });
-
 io.on('connection', onSocket);
 io.on('connection', onMeeting);
-
 mongoose
   .connect(MONGO_URL)
   .then(a => {
