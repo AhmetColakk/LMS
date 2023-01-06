0ba0b67f62470892fa1c57294ff051282aee0812
AhmetColakk <pxahmetcolak@gmail.com>
2023-01-06 02:49:09 +0200

    eight commit

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
+const path = require('path');
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const studentRouter = require('./routes/studentRouter');
 const announcementRouter = require('./routes/announcementRouter');
 const teacherRouter = require('./routes/teacherRouter');
 const studentAuhtRouter = require('./routes/authStudent');
-const { Server } = require('socket.io');
+const socket = require('socket.io');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const onSocket = require('./socket/socket');
 const onMeeting = require('./socket/meeting');
-const path = require('path');
 
-// ? initinilize app
-const app = express();
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
 
-// ? set graphql
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
 
-// ? Middlewares
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -45,22 +50,16 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URL = process.env.MONGO_URL;
-
-const io = new Server(4000, {
-  cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  },
-});
 io.on('connection', onSocket);
 io.on('connection', onMeeting);
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.MONGO_URL;
 mongoose
   .connect(MONGO_URL)
   .then(a => {
     // listen for request
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(
         `Connected to DB & Server is Listening on ${PORT}: http://localhost:${PORT}/`,
       );
